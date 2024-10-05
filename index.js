948333a09715dac90f65271a68bc9dd01634619a
praveen <nairxprv@gmail.com>
2024-10-05 12:20:52 +0530

    modifed

--- index.js
+++ index.js
@@ -12,9 +12,11 @@ const PASS = process.env.PASS
 const PORT = process.env.PORT || 8080;
 const usr = encodeURIComponent(DBUSER)
 const pwd = encodeURIComponent(PASS)
+// app.options('*', cors())
+app.use(cors())
 app.use("/users", userRouter);
 app.use("/todo", todoRouter);
-app.use(cors())
+
 mongoose
   .connect(
     `mongodb+srv://${usr}:${pwd}@cluster0.qjxhv.mongodb.net/todoapp?retryWrites=true&w=majority&appName=Cluster0`
