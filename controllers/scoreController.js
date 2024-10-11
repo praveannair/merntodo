078a5a442b8ad859d3e50417c81cfdbf27b65a8f
praveen <nairxprv@gmail.com>
2024-10-11 16:27:02 +0530

    added position

--- controllers/scoreController.js
+++ controllers/scoreController.js
@@ -17,7 +17,7 @@ const updateScore = async (req, res) => {
     const score = await scoreModel.findOne(
       { userId: req.userId },
       { _id: 0, score: 1 }
-    ); 
+    );
     res.status(200).json(score);
   } catch (error) {
     console.log(error);
@@ -31,7 +31,7 @@ const getScore = async (req, res) => {
       { userId: req.userId },
       { _id: 0, score: 1 }
     );
-    if (score.length === 0) res.status(200).json({score:0});
+    if (score.length === 0) res.status(200).json({ score: 0 });
     else res.status(200).json(score);
   } catch (error) {
     console.log(error);
@@ -39,4 +39,29 @@ const getScore = async (req, res) => {
   }
 };
 
-module.exports = { getScore, updateScore };
+const getPosition = async (req, res) => {
+  try {
+    // const position = await scoreModel.find({},{ _id: 0, userId:1,score: 1 }).sort({score:-1});
+
+
+   const position = await  scoreModel.aggregate([
+      {
+        $lookup: {
+          from: "users",
+          localField: "userId",
+          foreignField: "_id",
+          pipeline: [{$project:{username:1,email:1,_id:0}}],
+          as: "user",
+        },
+      },
+    ]);
+
+
+    res.status(200).json(position);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { getScore, updateScore, getPosition };
--- routes/scoreRoutes.js
+++ routes/scoreRoutes.js
@@ -2,12 +2,15 @@ const express = require("express");
 const {
   getScore,
   updateScore,
+  getPosition,
 } = require("../controllers/scoreController");
 const auth = require("../middlewares/auth");
 const authorize = require("../middlewares/authorize");
 const scoreRouter = express.Router();
 
 scoreRouter.get("/", auth, getScore);
+
+scoreRouter.get("/position", auth, getPosition);
 
 scoreRouter.post("/", auth, updateScore);
 
